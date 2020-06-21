6bea90f837762848972348dc60b9b63c0e740461
Ewunet Kassa <ewunetkassa@gmail.com>
2020-06-21 12:27:57 -0400

    button can fetch the data

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,24 @@
 $(document).ready(function () {
-  $("#btn").click(function () {
-    $.get("https://aimtell.com/quiz/sites.json/", "jsonp", function (
-      data,
-      status
-    ) {
-      console.log(data);
-      var data = JSON.parse(data);
-
-      data.forEach(function (dt) {
-        $("#tdata").append(
-          "<tr>" + "<td>" + dt.id + "</td>",
-          "<td>" + dt.name + "</td>",
-          "<td>" + dt.url + "</td>" + "</tr>"
-        );
-      });
-    });
+  $("button").click(function () {
+    $(".container").load("https://aimtell.com/quiz/sites.json/", "jasonp");
   });
+});
+
+$.ajax({
+  url: queryURL,
+  method: "GET",
+}).then(function (response) {
+  // Create a new table row element
+  var tRow = $("<tr>");
+
+  // Methods run on jQuery selectors return the selector i run on
+  // This is why I can create and save a reference to a td in the same statement i update its text
+  var idTd = $("<td>").text(response.id);
+  var nameTd = $("<td>").text(response.nameTd);
+  var urlTd = $("<td>").text(response.url);
+
+  // Append the newly created table data to the table row
+  tRow.append(idTd, nameTd, urlTd);
+  // Append the table row to the table body
+  $("tbody").append(tRow);
 });
