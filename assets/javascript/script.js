91e91ba1dcabe0a328a229d355790b4f14ec947c
Ewunet Kassa <ewunetkassa@gmail.com>
2020-09-15 14:57:02 -0400

    Update script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $("button").click(function () {
-    $(".container").load("https://aimtell.com/quiz/sites.json/", "jasonp");
+    $(".container").load("https://aimtell.com/quiz/sites.json/", "jsonp");
   });
 });
 
